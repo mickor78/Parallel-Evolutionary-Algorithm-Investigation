f33ed6925c4530831c622e55b6e44a233ff0a4d7
Michał Korzeniewski <michal.korzeniewski@rb.com>
2019-10-28 21:57:00 +0100

    Create project structure. Calculate first function.

---
+++ src/main/java/zad5/ppor/weiti/pw/edu/pl/Constants.java
@@ -0,0 +1,23 @@
+package zad5.ppor.weiti.pw.edu.pl;
+
+public class Constants {
+
+    public static class Range {
+        public static final double MAX_ELEMENT_IN_POPULATION = 40;
+        public static final double MIN_ELEMENT_IN_POPULATION = -40;
+        Range() {
+            throw new AssertionError();
+        }
+    }
+
+    public static class Cases {
+        public static final int ELEMENTS_NUM_IN_2_EL_POPULATION = 2;
+        public static final int ELEMENTS_NUM_IN_10_EL_POPULATION = 10;
+        public static final int ELEMENTS_NUM_IN_20_EL_POPULATION = 20;
+        public static final int ELEMENTS_NUM_IN_50_EL_POPULATION = 50;
+        public static final int ELEMENTS_NUM_IN_100_EL_POPULATION = 100;
+        Cases() {
+            throw new AssertionError();
+        }
+    }
+}
