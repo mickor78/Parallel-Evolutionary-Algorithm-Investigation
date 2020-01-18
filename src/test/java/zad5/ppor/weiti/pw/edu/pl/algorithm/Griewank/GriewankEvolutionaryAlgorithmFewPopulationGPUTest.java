7dfbafcb145b9635e1d3f46a01100a172cc0ad0c
Kamila Kulas <kam.kulas@gmail.com>
2020-01-18 13:36:10 +0100

    Another tests for GPU versions.

---
+++ src/test/java/zad5/ppor/weiti/pw/edu/pl/algorithm/Griewank/GriewankEvolutionaryAlgorithmFewPopulationGPUTest.java
@@ -0,0 +1,175 @@
+package zad5.ppor.weiti.pw.edu.pl.algorithm.Griewank;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import zad5.ppor.weiti.pw.edu.pl.algorithm.EvolutionaryAlgorithmFewPopulations;
+import zad5.ppor.weiti.pw.edu.pl.functions.GriewankFunctionGPU;
+import zad5.ppor.weiti.pw.edu.pl.model.Population;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.function.Function;
+
+@RunWith(Parameterized.class)
+
+public class GriewankEvolutionaryAlgorithmFewPopulationTest {
+
+
+    Population x;
+    Function<double[], Double> function;
+    int numberOfThreedToForkForAlgOperations;
+    long executionTime;
+    int numberOfThreedAimingByPopulations;
+    int numOfDivide;
+
+
+    public GriewankEvolutionaryAlgorithmFewPopulationTest(int numberOfThreedToForkForAlgOperations, int numberOfThreedAimingByPopulations, int numOfDivide, Population x) {
+        this.numberOfThreedToForkForAlgOperations = numberOfThreedToForkForAlgOperations;
+        this.numberOfThreedAimingByPopulations = numberOfThreedAimingByPopulations;
+        this.numOfDivide = numOfDivide;
+        this.x = x;
+    }
+
+    @BeforeClass
+    public static void before() {
+        System.out.println("pop num\t\tfeat\t\tthread_alg\t\tthread_pop\t\tnum_of_div\t\tmean\t\tvariance");
+
+    }
+
+    @Parameterized.Parameters
+    public static Collection parameters() {
+        Population pop_10000_100 = new Population(10000, 100);
+
+        Population pop_10000_10 = new Population(10000, 10);
+        Population pop_10000_50 = new Population(10000, 50);
+        return Arrays.asList(new Object[][]{
+                {1,     1,  4, pop_10000_10},
+                {1,     4,  4, pop_10000_10},
+                {2,     6,  4, pop_10000_10},
+                {4,     6,  4, pop_10000_10},
+                {4,     8,  4, pop_10000_10},
+
+                {2,     2,  4, pop_10000_10},
+
+                {4,     2,  4, pop_10000_10},
+                {2,     4,  4, pop_10000_10},
+
+                {2,     4,  8, pop_10000_10},
+                {4,     2,  8, pop_10000_10},
+
+                {2,     4,  10, pop_10000_10},
+                {4,     2,  10, pop_10000_10},
+
+                {2,     4,  20, pop_10000_10},
+                {4,     2,  20, pop_10000_10},
+
+
+                {1,     16,  4, pop_10000_10},
+                {16,    1,   4, pop_10000_10},
+                {2,     8,   4, pop_10000_10},
+                {8,     2,   4, pop_10000_10},
+                {4,     4,   4, pop_10000_10},
+
+
+
+
+                {1,     1,  4, pop_10000_100},
+                {1,     4,  4, pop_10000_100},
+                {2,     6,  4, pop_10000_100},
+                {4,     6,  4, pop_10000_100},
+                {4,     8,  4, pop_10000_100},
+
+                {2,     2,  4, pop_10000_100},
+
+                {4,     2,  4, pop_10000_100},
+                {2,     4,  4, pop_10000_100},
+
+                {2,     4,  8, pop_10000_100},
+                {4,     2,  8, pop_10000_100},
+
+                {2,     4,  10, pop_10000_100},
+                {4,     2,  10, pop_10000_100},
+
+                {2,     4,  20, pop_10000_100},
+                {4,     2,  20, pop_10000_100},
+
+
+                {1,     16,  4, pop_10000_100},
+                {16,    1,   4, pop_10000_100},
+                {2,     8,   4, pop_10000_100},
+                {8,     2,   4, pop_10000_100},
+                {4,     4,   4, pop_10000_100},
+
+
+
+                {1,     1,  4, pop_10000_50},
+                {1,     4,  4, pop_10000_50},
+                {2,     6,  4, pop_10000_50},
+                {4,     6,  4, pop_10000_50},
+                {4,     8,  4, pop_10000_50},
+
+                {2,     2,  4, pop_10000_50},
+
+                {4,     2,  4, pop_10000_50},
+                {2,     4,  4, pop_10000_50},
+
+                {2,     4,  8, pop_10000_50},
+                {4,     2,  8, pop_10000_50},
+
+                {2,     4,  10, pop_10000_50},
+                {4,     2,  10, pop_10000_50},
+
+                {2,     4,  20, pop_10000_50},
+                {4,     2,  20, pop_10000_50},
+
+
+                {1,     16,  4, pop_10000_50},
+                {16,    1,   4, pop_10000_50},
+                {2,     8,   4, pop_10000_50},
+                {8,     2,   4, pop_10000_50},
+                {4,     4,   4, pop_10000_50},
+        });
+    }
+
+    @Before
+    public void init() {
+        executionTime = 30;
+        function = new GriewankFunctionGPU();
+    }
+
+    @Test
+    public void findBestGenotype() {
+        EvolutionaryAlgorithmFewPopulations eafp = new EvolutionaryAlgorithmFewPopulations(
+                function, numberOfThreedToForkForAlgOperations, numOfDivide
+        );
+
+        double [] result = new double[5];
+        double mean = 0;
+        double variance = 0;
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = eafp.findBestGenotype(x, Double.NEGATIVE_INFINITY, executionTime).getRate();
+            mean += result[i];
+        }
+
+        mean/=result.length;
+
+        for (int i = 0; i < result.length; i++) {
+        variance += (result[i]-mean)*(result[i]-mean);
+        }
+        variance/=result.length;
+
+
+        System.out.println(
+                x.getPopulation().size() + "\t" +
+                        x.getPopulation().get(0).getGenotype().length + "\t" +
+                        numberOfThreedToForkForAlgOperations + "\t" +
+                        numberOfThreedAimingByPopulations+"\t"+
+                        numOfDivide+"\t"+
+                        mean+ "\t" +variance);
+    }
+}
---
+++ src/test/java/zad5/ppor/weiti/pw/edu/pl/algorithm/Griewank/GriewankEvolutionaryAlgorithmOnePopulationGPUTest.java
@@ -0,0 +1,103 @@
+package zad5.ppor.weiti.pw.edu.pl.algorithm.Griewank;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import zad5.ppor.weiti.pw.edu.pl.algorithm.EvolutionaryAlgorithmOnePopulation;
+import zad5.ppor.weiti.pw.edu.pl.functions.GriewankFunctionGPU;
+import zad5.ppor.weiti.pw.edu.pl.model.Population;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.function.Function;
+
+@RunWith(Parameterized.class)
+
+
+public class GriewankEvolutionaryAlgorithmOnePopulationTest {
+
+
+    Population x;
+    Function<double[],Double> function;
+    int numberOfThreedToFork;
+    long executionTime;
+
+
+    @BeforeClass
+    public static void before(){
+        System.out.println("pop num\t\tfeat\t\ttthread\t\tmean\t\tvariance");
+    }
+    @Before
+    public void init() {
+        executionTime = 30;
+        function = new GriewankFunctionGPU();
+    }
+
+    public GriewankEvolutionaryAlgorithmOnePopulationTest(Integer numberOfThreedToFork, Population x) {
+        this.x = x;
+        this.numberOfThreedToFork = numberOfThreedToFork;
+    }
+
+    @Parameterized.Parameters
+    public static Collection parameters() {
+        Population pop_10000_100 = new Population(10000, 100);
+
+        Population pop_10000_10 = new Population(10000, 10);
+        Population pop_10000_50 = new Population(10000, 50);
+        return Arrays.asList(new Object[][] {
+                { 1, pop_10000_100},
+//                { 2, pop_10000_100},
+//                { 4, pop_10000_100},
+//                { 8, pop_10000_100},
+//                { 12, pop_10000_100},
+//                { 16, pop_10000_100},
+//                { 20, pop_10000_100},
+                { 1, pop_10000_10},
+//                { 2, pop_10000_10},
+//                { 4, pop_10000_10},
+//                { 8, pop_10000_10},
+//                { 12, pop_10000_10},
+//                { 16, pop_10000_10},
+//                { 20, pop_10000_10},
+                { 1, pop_10000_50},
+//                { 2, pop_10000_50},
+//                { 4, pop_10000_50},
+//                { 8, pop_10000_50},
+//                { 12, pop_10000_50},
+//                { 16, pop_10000_50},
+//                { 20, pop_10000_50}
+        });
+    }
+
+    @Test
+    public void findBestGenotype() {
+        EvolutionaryAlgorithmOnePopulation eaop = new EvolutionaryAlgorithmOnePopulation(
+                numberOfThreedToFork, function
+        );
+        double [] result = new double[5];
+        double mean = 0;
+        double variance = 0;
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = eaop.findBestGenotype(x, Double.NEGATIVE_INFINITY, executionTime).getRate();
+            mean += result[i];
+        }
+
+        mean/=result.length;
+
+        for (int i = 0; i < result.length; i++) {
+            variance += (result[i]-mean)*(result[i]-mean);
+        }
+        variance/=result.length;
+
+
+        System.out.println(
+                x.getPopulation().size() + "\t" +
+                        x.getPopulation().get(0).getGenotype().length + "\t" +
+                        numberOfThreedToFork + "\t" +
+                        mean+ "\t" +variance);
+    }
+}
